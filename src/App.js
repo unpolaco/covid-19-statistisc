efdddca521529b8cd00baa398f26df41f83b5bda
Michal Borowski <unpolacoo@gmail.com>
2020-05-08 23:33:00 +0200

    Add graphql request for data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import './App.css';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+const get_test_info = gql`
+	{
+		results(countries: [], date: { eq: "5/07/2020" }) {
+			country {
+				name
+			}
+			confirmed
+			date
+			deaths
+			recovered
+			growthRate
+		}
+	}
+`;
 
 function App() {
-	return <div className='App'></div>;
+	const { data, loading, error } = useQuery(get_test_info);
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error</p>;
+	console.log(data);
+
+
+
+	return (
+		<div className='App'>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'https://covid19-graphql.now.sh'
+})
+const client = new ApolloClient({
+  cache,
+  link
+})
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
 
 serviceWorker.unregister();
