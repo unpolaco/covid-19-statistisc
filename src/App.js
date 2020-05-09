14abfafb58fc8c7d467180357e0b2e83fe3f0e89
Michal Borowski <unpolacoo@gmail.com>
2020-05-09 10:18:18 +0200

    Add getMapData request to map component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import React from 'react';
 import Map from './components/Map';
 import './App.css';
-import { useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
-const get_test_info = gql`
-{
-  results(countries: [], date: { eq: "5/07/2020" }) {
-    country {
-      name
-    }
-    confirmed
-    date
-    deaths
-    recovered
-    growthRate
-  }
-}
-`;
+
 
 function App() {
-  const { data, loading, error } = useQuery(get_test_info);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error</p>;
-  console.log(data);
-  
-  const mapData = data.results.map((d) => {
-    return { value: d.confirmed, id: d.country.name };
-  });
-    
-    
     return (
       <div className='App'>
-      <Map data={mapData} />
+      <Map/>
 		</div>
 	);
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,55 +1,81 @@
 import React from 'react';
 import { ResponsiveChoropleth } from '@nivo/geo';
 import countries from '../assets/world_countries.json';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
-const MyResponsiveChoropleth = ({ data }) => (
-	<ResponsiveChoropleth
-		data={data}
-		width={1000}
-		height={700}
-		features={countries.features}
-		margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-		//   colors={['red', 'green', 'blue']}
-		colors='YlOrBr'
-		//   domain={[ 0, Math.max(...data.map(d => (d.value)))]}
-		domain={[0, 300000]}
-		unknownColor='#e9e9e9'
-		label='data.id'
-		valueFormat='.2s'
-		projectionType='naturalEarth1'
-		projectionTranslation={[0.5, 0.5]}
-		projectionRotation={[0, 0, 0]}
-		projectionScale={277}
-		enableGraticule={true}
-		graticuleLineColor='#dddddd'
-		borderWidth={1}
-		borderColor='#283a44'
-		legends={[
-			{
-				anchor: 'bottom-left',
-				direction: 'column',
-				justify: false,
-				translateX: 22,
-				translateY: -14,
-				itemsSpacing: 0,
-				itemWidth: 94,
-				itemHeight: 18,
-				itemDirection: 'left-to-right',
-				itemTextColor: '#444444',
-				itemOpacity: 0.85,
-				symbolSize: 15,
-				effects: [
-					{
-						on: 'hover',
-						style: {
-							itemTextColor: '#000000',
-							itemOpacity: 1,
+const getMapData = gql`
+	{
+		results(countries: [], date: { eq: "5/07/2020" }) {
+			country {
+				name
+			}
+			confirmed
+			date
+			deaths
+			recovered
+			growthRate
+		}
+	}
+`;
+
+function MyResponsiveChoropleth() {
+	const { data, loading, error } = useQuery(getMapData);
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error</p>;
+	console.log(data);
+
+	const mapData = data.results.map((d) => {
+		return { value: d.confirmed, id: d.country.name };
+	});
+
+	return (
+		<ResponsiveChoropleth
+			data={mapData}
+			width={1000}
+			height={700}
+			features={countries.features}
+			margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+			colors='YlOrBr'
+			domain={[0, 300000]}
+			unknownColor='#e9e9e9'
+			label='data.id'
+			valueFormat='.2s'
+			projectionType='naturalEarth1'
+			projectionTranslation={[0.5, 0.5]}
+			projectionRotation={[0, 0, 0]}
+			projectionScale={277}
+			enableGraticule={true}
+			graticuleLineColor='#dddddd'
+			borderWidth={1}
+			borderColor='#283a44'
+			legends={[
+				{
+					anchor: 'bottom-left',
+					direction: 'column',
+					justify: false,
+					translateX: 22,
+					translateY: -14,
+					itemsSpacing: 0,
+					itemWidth: 94,
+					itemHeight: 18,
+					itemDirection: 'left-to-right',
+					itemTextColor: '#444444',
+					itemOpacity: 0.85,
+					symbolSize: 15,
+					effects: [
+						{
+							on: 'hover',
+							style: {
+								itemTextColor: '#000000',
+								itemOpacity: 1,
+							},
 						},
-					},
-				],
-			},
-		]}
-	/>
-);
+					],
+				},
+			]}
+		/>
+	);
+}
 
 export default MyResponsiveChoropleth;
