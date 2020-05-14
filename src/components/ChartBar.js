18b1feebc170c73af5eb858a7a62d7783ef96cf2
Michal Borowski <unpolacoo@gmail.com>
2020-05-15 00:59:32 +0200

    Add time range radio button

--- src/components/ChartBar.js
+++ src/components/ChartBar.js
@@ -3,13 +3,9 @@ import { ResponsiveBar } from '@nivo/bar';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import 'moment/locale/pl';
+import moment from 'moment';
 
 function ChartBar() {
-	const [requestCountry, setCountry] = useState('Poland');
-	const [selectedCase, setCase] = useState('confirmed');
-	const countryInput = useRef(null);
-
-	// console.log(requestCountry);
 	const casesNames = [
 		{ displayName: 'Confirmed', value: 'confirmed' },
 		{ displayName: 'New Confirmed', value: 'newConfirmed' },
@@ -18,10 +14,24 @@ function ChartBar() {
 		{ displayName: 'Recovered', value: 'recovered' },
 		{ displayName: 'New Recovered', value: 'newRecovered' },
 	];
+	const now = moment();
+	const fromMarch = '03/01/2020';
+	const lastMonth = now.clone().subtract(1, 'month').format('MM/DD/YYYY');
+
+	console.log(fromMarch, lastMonth,'now');
+	const timeRange = [
+		{ displayTime: 'last week', value: fromMarch },
+		{ displayTime: 'last month', value: lastMonth },
+	];
+
+	const [requestCountry, setCountry] = useState('Poland');
+	const [selectedCase, setCase] = useState('confirmed');
+	const [selectedTimeRange, setTimeRange] = useState(fromMarch);
+	const countryInput = useRef(null);
 
 	const getMapData = gql`
 		{
-			results(countries: [ "${requestCountry}" ], date: { gt: "3/20/2020" }) {
+			results(countries: [ "${requestCountry}" ], date: { gt: "${selectedTimeRange}" }) {
 				country {
 					name
 				}
@@ -59,6 +69,10 @@ function ChartBar() {
 		setCase(e.target.value);
 	}
 
+	function handleClickTimeRange(e) {
+		setTimeRange(e.target.value);
+	}
+
 	return (
 		<>
 			<input id='country' ref={countryInput}></input>
@@ -68,12 +82,19 @@ function ChartBar() {
 			</button>
 
 			{casesNames.map((el) => (
-				<button onClick={handleClickCases} 
-								value={el.value} 
-								key={el.value}>
+				<button onClick={handleClickCases} value={el.value} key={el.value}>
 					{el.displayName}
 				</button>
 			))}
+
+			<form >
+			{timeRange.map((el) => (
+				<>
+				<input type='radio' name={timeRange} onClick={handleClickTimeRange} value={el.value} key={el.value} id={el.value}></input>
+				<label htmlFor={el.value} key={el.value + 'label'}>{el.displayTime}</label>
+				</>
+			))}
+			</form>
 
 			<ResponsiveBar
 				data={chartData}
