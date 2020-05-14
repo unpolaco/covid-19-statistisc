20cd1721738e619555bba1a9e0791c22764a8df1
Michal Borowski <unpolacoo@gmail.com>
2020-05-14 23:31:05 +0200

    Delete chart legend

--- src/components/ChartBar.js
+++ src/components/ChartBar.js
@@ -104,30 +104,6 @@ function ChartBar() {
 				labelSkipWidth={15}
 				labelSkipHeight={15}
 				labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-				legends={[
-					{
-						dataFrom: 'keys',
-						anchor: 'bottom-right',
-						direction: 'column',
-						justify: false,
-						translateX: 120,
-						translateY: 0,
-						itemsSpacing: 2,
-						itemWidth: 100,
-						itemHeight: 20,
-						itemDirection: 'left-to-right',
-						itemOpacity: 0.85,
-						symbolSize: 20,
-						effects: [
-							{
-								on: 'hover',
-								style: {
-									itemOpacity: 1,
-								},
-							},
-						],
-					},
-				]}
 				animate={true}
 				motionStiffness={115}
 				motionDamping={15}
