65590c74a8861c867852ae74517d486fb602127d
Klara Borowska <kborowska@catalogicsoftware.com>
2020-05-17 21:40:52 +0200

    add  sorting function

--- src/components/SummaryTable.js
+++ src/components/SummaryTable.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
+import moment from 'moment';
  
 const getData = (data) => {
   return gql`
@@ -20,56 +21,112 @@ const getData = (data) => {
 }
 
 const SummaryTable = () => {
-  let dataToRender = [];
-  const lastDayData = useQuery(getData('5/08/2020'));
-  const prevDayData = useQuery(getData('5/07/2020'));
+  const lastDay = moment().subtract(2, 'day').format('M/DD/YYYY');
+  const prevDay = moment().subtract(3, 'day').format('M/DD/YYYY');
+  const lastDayData = useQuery(getData(lastDay));
+  const prevDayData = useQuery(getData(prevDay));
+  
+  const [ dataToRender, setData ] = useState([]);
+  const [ casesDetails ] = useState([
+    { displayName: 'Country', value: ['country', 'name'], type: 'string', asc: null },
+    { displayName: 'Confirmed', value: ['confirmed'], type: 'number', asc: null },
+    { displayName: 'New Confirmed', value: ['newConfirmed'], type: 'number', asc: null },
+    { displayName: 'Deaths', value: ['deaths'], type: 'number', asc: null },
+    { displayName: 'New Deaths', value: ['newDeaths'], type: 'number', asc: null },
+    { displayName: 'Recovered', value: ['recovered'], type: 'number', asc: null },
+    { displayName: 'Active', value: ['active'], type: 'number', asc: null },
+    { displayName: 'Death Rate', value: ['deathRate'], type: 'number', asc: null }
+  ]);
 
   const prepareDataToRender = (lastData, compareData) => {
+    const assignIfPositive = (newData) => {
+      return newData > 0 ? newData : null;
+    }
+    const data = [...lastData];
     lastData.forEach((el, index)=>{
-      el.newConfirmed = el.confirmed - compareData[index].confirmed;
-      el.newDeaths = el.deaths - compareData[index].deaths;
-      el.active = el.confirmed - el.deaths - el.recovered;
-      const padding = 10;
-      el.deathRate = Math.round((el.deaths / el.confirmed) * 100 * padding) / padding;
+      el.newConfirmed = assignIfPositive(el.confirmed - compareData[index].confirmed);
+      el.newDeaths = assignIfPositive(el.deaths - compareData[index].deaths);
+      el.active = assignIfPositive(el.confirmed - el.deaths - el.recovered);
+      el.deathRate = assignIfPositive(+((el.deaths / el.confirmed) * 100).toFixed(1));
     });
-    return lastData;
+    sortData(data, casesDetails[0]);
+    return data;
   }
-  
-  if (lastDayData.data && prevDayData.data) {
-    dataToRender = prepareDataToRender(lastDayData.data.results, prevDayData.data.results);
+
+  const sortData = (array, column) => {
+    const keys = column.value;
+    const columnType = column.type;
+    const ascSort = column.asc;
+    
+    array.sort((a,b) => {
+      const firstKey = keys[0];
+      const secondKey = keys[1] || null;
+      const firstEl = secondKey ? a[firstKey][secondKey] : a[firstKey];
+      const secondEl = secondKey ? b[firstKey][secondKey] : b[firstKey];
+      
+      if (columnType === 'string') {
+        if (firstEl.toLowerCase() > secondEl.toLowerCase()) {
+          return ascSort ? -1 : 1;
+        } else if (secondEl.toLowerCase() > firstEl.toLowerCase()) {
+          return ascSort ? 1 : -1;
+        } else {
+          return 0;
+        }
+      } else if (columnType === 'number') {
+        return ascSort ? firstEl - secondEl : secondEl - firstEl;
+      }
+    });
+    for (let el of casesDetails) {
+      el.asc = el.displayName == column.displayName ? !el.asc : null;
+    }
+    setData([...array]);
   }
+
+  useEffect(() => {
+    if (lastDayData.data && prevDayData.data) {
+      const newData = prepareDataToRender(lastDayData.data.results, prevDayData.data.results);    
+      setData(newData);
+    }
+  }, [lastDayData.data, prevDayData.data]);
   
-  if (lastDayData.loading) return <p>Loading...</p>;
-  if (lastDayData.error) return <p>Error</p>;
+  const sortOnClick = (column) => {
+    sortData(dataToRender, column);
+  }
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Country</th>
-          <th>Confirmed</th>
-          <th>New Confirmed</th>
-          <th>Deaths</th>
-          <th>New Deaths</th>
-          <th>Recovered</th>
-          <th>Active</th>
-          <th>Death Rate</th>
-        </tr>
-      </thead>
-      <tbody>
-        {dataToRender.map((el, index)=>{
-          return <tr key={index}>
-                    <td>{el.country.name}</td>
-                    <td>{el.confirmed || ''}</td>
-                    <td>{el.newConfirmed ? '+' + el.newConfirmed : ''}</td>
-                    <td>{el.deaths || ''}</td>
-                    <td>{el.newDeaths ? '+' + el.newDeaths : ''}</td>
-                    <td>{el.recovered || ''}</td>
-                    <td>{el.active || ''}</td>
-                    <td>{el.deathRate ? el.deathRate + '%' : ''}</td>
-                  </tr>
-        })}
-      </tbody>
-    </table>
+    <div>
+      {dataToRender.length > 0 &&
+      <table>
+        <thead>
+          <tr>
+            {casesDetails.map((el, index)=>{
+              return <th key={index} onClick={()=>sortOnClick(el)} className={'cursor-pointer'}>
+                      {el.displayName}
+                      <button className={'btn-icon'}>
+                        {el.asc && <span>&#x25BE;</span>}
+                        {el.asc === false && <span>&#x25B4;</span>}
+                      </button>
+                    </th>
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {dataToRender.map((element, index)=>{
+            return <tr key={index}>
+              {casesDetails.map((el, index)=>{
+                const firstKey = el.value[0];
+                const secondKey = el.value[1];
+                return <td key={index+firstKey}>
+                        {el.displayName.toLowerCase().includes('new') && element[firstKey] > 0 &&  <span>+</span>}
+                        {(secondKey ? element[firstKey][secondKey] : element[firstKey]) || ''}
+                        {el.displayName.toLowerCase().includes('rate') && element[firstKey] > 0 && <span>%</span>}
+                      </td>
+              })}        
+            </tr>
+          })}
+        </tbody>
+      </table>}
+    </div>
   );
 }
 
