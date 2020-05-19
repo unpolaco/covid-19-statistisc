9efdd6c4bb28bdf41c4d6f554bf9d354da7fe078
Michal Borowski <unpolacoo@gmail.com>
2020-05-19 09:34:16 +0200

    Add map function for buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default function Button(props) {
 
   return(
-    <button onClick={props.handleClick}>{props.text}</button>
+    <button onClick={props.handleClick} value={props.value} name={props.name}>{props.name}</button>
   )
 }
