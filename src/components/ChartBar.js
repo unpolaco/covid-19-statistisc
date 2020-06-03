a2026247fb68f4b0d57dfa17daebc684c3350497
Michal Borowski <unpolacoo@gmail.com>
2020-06-03 14:49:03 +0200

    Get all data dates at begining then filter for smooth transition

--- src/components/ChartBar.js
+++ src/components/ChartBar.js
@@ -6,6 +6,7 @@ import 'moment/locale/pl';
 import moment from 'moment';
 import countries from '../assets/world_countries.json';
 import styles from './ChartBar.module.scss';
+import InputRadioTimeRange from './Input_Radio_TimeRange';
 
 function ChartBar() {
 	const casesNames = [
@@ -17,31 +18,32 @@ function ChartBar() {
 		{ displayName: 'New Recovered', value: 'newRecovered' },
 	];
 	const now = moment();
-	const fromJanuary = '12/01/2019';
-	const fromMarch = '03/01/2020';
-	const lastMonth = now.clone().subtract(1, 'month').format('MM/DD/YYYY');
+	const fromJanuary = '2020-01-01';
+	const pandemicStart = '2020-03-11';
+	const lastMonth = now.clone().subtract(1, 'month').format('YYYY-MM-DD');
 
 	const timeRange = [
 		{ displayTime: 'from 1 January', value: fromJanuary },
-		{ displayTime: 'from 1 March', value: fromMarch },
+		{ displayTime: 'from 11 March', value: pandemicStart },
 		{ displayTime: 'last month', value: lastMonth },
 	];
+
 	const countryList = [];
-	const l = countries.features.length
+	const l = countries.features.length;
 	for (let i = 0; i < l; i++) {
 		countryList.push(countries.features[i].id);
 	}
 
+	const [selectedTimeRangeOption, setTimeRangeOption] = useState(pandemicStart);
 	const [selectedCountry, setCountry] = useState('Poland');
 	const [selectedCase, setCase] = useState('confirmed');
-	const [selectedTimeRange, setTimeRange] = useState(fromMarch);
-
 	const [textValue, setTextValue] = useState('');
 	const countryInput = useRef(null);
 
 	const getMapData = gql`
-		{
-			results(countries: [ "${selectedCountry}" ], date: { gt: "${selectedTimeRange}" }) {
+	{
+		results(countries: [ "${selectedCountry}" ], 
+		date: { gt: "01/01/2020" }) {
 				country {
 					name
 				}
@@ -70,6 +72,11 @@ function ChartBar() {
 		return d;
 	});
 
+	const timeFilterChartData = chartData.filter((d, index) => {
+		if (moment(chartData[index].date).isAfter(moment(selectedTimeRangeOption)))
+			return d;
+	});
+
 	function handleClickCountry() {
 		setCountry(countryInput.current.value);
 		refetch();
@@ -79,9 +86,6 @@ function ChartBar() {
 		setCase(e.target.value);
 	}
 
-	function handleClickTimeRange(e) {
-		setTimeRange(e.target.value);
-	}
 	function handleFilterCountryList(e) {
 		setTextValue(e.target.value);
 	}
@@ -89,88 +93,91 @@ function ChartBar() {
 		setCountry(e.target.innerText);
 		refetch();
 	}
+	function onChangeTimeRange(selectedTimeRangeOption) {
+		setTimeRangeOption(selectedTimeRangeOption);
+	}
 
 	return (
-		<section>
-			<h2 id='chartBar'>{selectedCountry}</h2>
-			<div className='searchWrapper'>
-			<div className='inputWrapper'>
-				<input
-					id='country'
-					onChange={handleFilterCountryList}
-					ref={countryInput}
-					autocomplete='off'
-				></input>
-				<label htmlFor='country'>Country name</label>
-				<button type='submit' htmlFor='country' onClick={handleClickCountry}>
-					Search
-				</button>
-			</div>
-				<ul className='countryList'>
+		<section id='chartBar' className={styles.section_wrapper}>
+			<h1 className={styles.country_name}>{selectedCountry}</h1>
+			<div className={styles.vertical_line}></div>
+			<div className={styles.search_wrapper}>
+				<div className={styles.input_wrapper}>
+					<input
+						id='country'
+						onChange={handleFilterCountryList}
+						ref={countryInput}
+						autoComplete='off'
+					></input>
+					<label htmlFor='country'>Country name</label>
+					<button type='submit' htmlFor='country' onClick={handleClickCountry}>
+						Search
+					</button>
+				</div>
+				<ul className={styles.country_list}>
 					{countryList
 						.filter((name) => {
 							return name.toUpperCase().includes(textValue.toUpperCase());
 						})
 						.map((filteredName) => (
-							<li onClick={handleClickCountryList}>{filteredName}</li>
+							<li
+								onClick={handleClickCountryList}
+								key={filteredName + 'countryList'}
+							>
+								{filteredName}
+							</li>
 						))}
 				</ul>
 			</div>
 
-			{casesNames.map((el) => (
-				<button onClick={handleClickCases} value={el.value} key={el.value}>
-					{el.displayName}
-				</button>
-			))}
-
-			{timeRange.map((el) => (
-				<form id={el.value + 'form'}>
-					<input
-						type='radio'
-						name={timeRange}
-						onClick={handleClickTimeRange}
+			<div className={styles.cases_container}>
+				{casesNames.map((el) => (
+					<button
+						onClick={handleClickCases}
 						value={el.value}
-						key={el.value}
-						id={el.value}
-					></input>
-					<label htmlFor={el.value} key={el.value + 'label'}>
-						{el.displayTime}
-					</label>
-				</form>
-			))}
+						key={el.value + 'casesNames'}
+					>
+						{el.displayName}
+					</button>
+				))}
+			</div>
 
-			<ResponsiveBar
-				data={chartData}
-				width={1000}
-				height={500}
-				keys={[selectedCase]}
-				indexBy='date'
-				margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-				padding={0.3}
-				groupMode='grouped'
-				minValue={0}
-				colors={{ scheme: 'category10' }}
-				borderColor={{ from: 'color', modifiers: [['darker', '1.6']] }}
-				axisBottom={{
-					tickSize: 5,
-					tickPadding: 5,
-					tickRotation: -45,
-					legendPosition: 'middle',
-					legendOffset: 32,
-				}}
-				axisLeft={{
-					tickSize: 5,
-					tickPadding: 5,
-					tickRotation: 0,
-					legendPosition: 'middle',
-					legendOffset: -40,
-				}}
-				labelSkipWidth={15}
-				labelSkipHeight={15}
-				labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-				animate={true}
-				motionStiffness={115}
-				motionDamping={15}
+			<div className={styles.chartbar_container}>
+				<ResponsiveBar
+					data={timeFilterChartData}
+					keys={[selectedCase]}
+					indexBy='date'
+					margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+					padding={0.3}
+					groupMode='grouped'
+					minValue={0}
+					colors={{ scheme: 'category10' }}
+					borderColor={{ from: 'color', modifiers: [['darker', '1.6']] }}
+					axisBottom={{
+						tickSize: 5,
+						tickPadding: 5,
+						tickRotation: -45,
+						legendPosition: 'middle',
+						legendOffset: 32,
+					}}
+					axisLeft={{
+						tickSize: 5,
+						tickPadding: 5,
+						tickRotation: 0,
+						legendPosition: 'middle',
+						legendOffset: -40,
+					}}
+					labelSkipWidth={15}
+					labelSkipHeight={15}
+					labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+					animate={true}
+					motionStiffness={115}
+					motionDamping={15}
+				/>
+			</div>
+			<InputRadioTimeRange
+				onChangeTimeRange={onChangeTimeRange}
+				timeRange={timeRange}
 			/>
 		</section>
 	);
