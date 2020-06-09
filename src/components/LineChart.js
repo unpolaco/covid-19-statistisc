36449aa3cdedb856b45a10b8247f3588bce14818
Michal Borowski <unpolacoo@gmail.com>
2020-06-09 10:57:46 +0200

    Add new cases for LineChart

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -3,39 +3,72 @@ import React, { useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import styles from './LineChart.module.scss';
+import casesNames from '../assets/cases_names';
 
 function LineChart() {
-	const [mainCountry, setMainCountry] = useState('Poland')
+	const [mainCountry, setMainCountry] = useState('Poland');
 	const [selectedCountry, setCountry] = useState({
 		France: false,
 		Spain: false,
-		Italy: false
-	})
+		Italy: false,
+	});
+	const [selectedCase, setCase] = useState('newConfirmed');
 
-	const countriesForLineChart = [ mainCountry ]
-	countriesForLineChart.push(...Object.keys(selectedCountry)) 
+	const countriesForLineChart = [mainCountry];
+	countriesForLineChart.push(...Object.keys(selectedCountry));
 
 	const getLineChartData = gql`
-	query countries($countriesNames: [String]!)
-	{countries(names: $countriesNames) {
-		name
-		results {
-			date
-			confirmed
-			deaths
-			recovered
+		query countries($countriesNames: [String]!) {
+			countries(names: $countriesNames) {
+				name
+				results {
+					date
+					confirmed
+					deaths
+					recovered
+				}
+			}
 		}
-	}}
 	`;
-	
-	const { data, loading, error } = useQuery(getLineChartData, {variables: {countriesNames: countriesForLineChart}})
+
+	const { data, loading, error } = useQuery(getLineChartData, {
+		variables: { countriesNames: countriesForLineChart },
+	});
 	if (loading) return <p>Loading...</p>;
 	if (error) return <p>Error</p>;
 
-	const dataForLineChart = data.countries.map( d => {
-		const values = d.results.map(value => ({x: value.date, y: value.confirmed}))
-		return {id : d.name, data: values}
-	})
+	const dataWithNewCases = data.countries.map((d, index) => {
+		const addNewCases = d.results.map((el, index) => {
+			if (index > 0) {
+				d.results[index].newConfirmed =
+					d.results[index].confirmed - d.results[index - 1].confirmed < 0
+						? 0
+						: d.results[index].confirmed - d.results[index - 1].confirmed;
+				d.results[index].newDeaths =
+					d.results[index].deaths - d.results[index - 1].deaths < 0
+						? 0
+						: d.results[index].deaths - d.results[index - 1].deaths;
+				d.results[index].newRecovered =
+					d.results[index].recovered - d.results[index - 1].recovered < 0
+						? 0
+						: d.results[index].recovered - d.results[index - 1].recovered;
+			} else if (index === 0) {
+				d.results[index].newConfirmed = 0;
+				d.results[index].newDeaths = 0;
+				d.results[index].newRecovered = 0;
+			}
+		});
+		return d;
+	});
+	const dataForLineChart = data.countries.map((d) => {
+		const values = d.results.map((value) => ({
+			x: value.date,
+			y: value[selectedCase],
+		}));
+		return { id: d.name, data: values };
+	});
+
+	console.log(dataForLineChart);
 
 	function handleClick(e) {
 		const newCountry = e.target.value;
@@ -43,24 +76,43 @@ function LineChart() {
 			setCountry(newCountry);
 			selectedCountry[newCountry] = true;
 		} else {
-			setCountry(newCountry)
+			setCountry(newCountry);
 		}
-		
 		console.log('This country is already on the chart!');
 	}
-	const buttons = []
-				for(let [key, value] of Object.entries(selectedCountry)) {	
-				buttons.push(<button onClick={handleClick} key={key + "linechartButton"} value={key} visible={value}>
+
+	function handleClickCases(e) {
+		setCase(e.target.value);
+	}
+	const buttons = [];
+	for (let [key, value] of Object.entries(selectedCountry)) {
+		buttons.push(
+			<button
+				onClick={handleClick}
+				key={key + 'linechartButton'}
+				value={key}
+				visible={value}
+			>
 				{key}
-			</button>)	
-				}
+			</button>
+		);
+	}
 
 	return (
 		<>
 			<div className={styles.wrapper}>
-				<div className={styles.button_container}>
-				{buttons}
+				<div className={styles.cases_container}>
+					{casesNames.map((el) => (
+						<button
+							onClick={handleClickCases}
+							value={el.value}
+							key={el.value + 'casesNames'}
+						>
+							{el.displayName}
+						</button>
+					))}
 				</div>
+				<div className={styles.button_container}>{buttons}</div>
 				<div className={styles.chart_container}>
 					<ResponsiveLine
 						data={dataForLineChart}
