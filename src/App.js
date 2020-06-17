8cc254f44b00f94ff66674a6339317abf3d1f25e
Michal Borowski <unpolacoo@gmail.com>
2020-06-17 23:44:13 +0200

    Add country context with useContext hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import Nav from './components/Nav';
+import Header from './components/Header';
 import CountryPage from './components/CountryPage';
 import GlobalPage from './components/GlobalPage';
 import styles from './App.module.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {CountryContextProvider} from './context/country_context'
+
 
 function App() {
 	return (
-		<div className={styles.App}>
+		<CountryContextProvider>
+		<div className={styles.app}>
 			<Router>
-				<Nav className={styles.header} />
+				{/* <Header className={styles.header} /> */}
 				<Switch>
 					<Route exact path='/' component={GlobalPage} />
 					<Route path='/countryPage' component={CountryPage} />
 				</Switch>
 			</Router>
 		</div>
+		</CountryContextProvider>
 	);
 }
 
---
+++ src/context/country_context.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+
+
+const CountryContext = React.createContext({
+  country: null,
+  setInputCountry: () => {}
+})
+
+const CountryContextProvider = (props) => {
+  const [countryPage, setCountryPage] = useState("Poland")
+
+  const countryInputHandler = (country) => {
+    setCountryPage(country)
+  }
+  return (
+    <CountryContext.Provider value={{ setInputCountry: countryInputHandler, country: countryPage}} >
+      {props.children}
+    </CountryContext.Provider>
+  )
+}
+export { CountryContext, CountryContextProvider }
