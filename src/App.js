b44c4bef1778b4c0866891ee1dd063bef3241dd4
Michal Borowski <unpolacoo@gmail.com>
2020-06-18 11:49:37 +0200

    Create ScrollToTop component for router

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import GlobalPage from './components/GlobalPage';
 import styles from './App.module.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import {CountryContextProvider} from './context/country_context'
-
+import ScrollToTop from './components/Scroll_To_Top'
 
 function App() {
 	return (
 		<CountryContextProvider>
 		<div className={styles.app}>
 			<Router>
-				{/* <Header className={styles.header} /> */}
+			<ScrollToTop />
 				<Switch>
 					<Route exact path='/' component={GlobalPage} />
 					<Route path='/countryPage' component={CountryPage} />
