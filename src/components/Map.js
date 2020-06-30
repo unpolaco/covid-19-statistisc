e2b4e4532d5c29d56bbad55b168860e7d5ea0f09
Michal Borowski <unpolacoo@gmail.com>
2020-06-30 15:32:49 +0200

    Add radio btn into map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,12 @@
 import React, { useState, useContext } from 'react';
 import { ResponsiveChoropleth as ResponsiveMap } from '@nivo/geo';
 import countries from '../assets/world_countries.json';
-import Button from './Button';
 import InputRange from './InputRange';
 import styles from './Map.module.scss';
 import colors from '../assets/colors'
-import casesNames from '../assets/cases_names'
 import { useHistory } from 'react-router-dom';
 import { CountryContext } from '../context/country_context';
+import InputRadioCasesMap from './Input_Radio_Cases_Map'
 
 const mapHeight = '500';
 const mapWidth = '850';
@@ -15,45 +14,41 @@ const mapWidth = '850';
 function Map({ data }) {
 	const history = useHistory();
 	const countryContext = useContext(CountryContext);
-	const [selectedCasesType, setCasesType] = useState('confirmed');
+	const [selectedCaseType, setCaseType] = useState('confirmed');
 	const [maxDomainValue, setMaxDomainValue] = useState(250000);
 
 	const mapData = data.countries.map((d) => {
-		return { value: d.mostRecent[selectedCasesType], id: d.name };
+		return { value: d.mostRecent[selectedCaseType], id: d.name };
 	});
 	const handleDomainValueChange = (e) => {
 		setMaxDomainValue(e.target.value);
-	};
-	const handleTypeChange = (e) => {
-		setCasesType(e.target.value);
-		setMaxDomainValue(e.target.value === 'deaths' ? 100000 : 250000);
 	};
 	const onMapClick = (country) => {
 		countryContext.setInputCountry(country);
 		history.push('/countryPage');
 	}
+	function onChangeCaseType(selectedCaseType) {
+		setCaseType(selectedCaseType);
+		setMaxDomainValue(selectedCaseType === 'deaths' ? 100000 : 250000);
+	}
 
 	return (
 		<section className={styles.section_map} id='map'>
-			<div className={styles.flex_buttons}>
-				{casesNames.map((el) => (
-					<Button
-						className={styles.button}
-						handleClick={(e) => handleTypeChange(e)}
-						value={el.value}
-						name={el.displayName}
-					/>
-				))}
-				<p className={styles.max_domain_value}>{maxDomainValue}</p>
-				<InputRange value={maxDomainValue} onChange={handleDomainValueChange} />
-			</div>
-			<p className={styles.section_title}>{`${selectedCasesType} cases`}</p>
+
+				<InputRadioCasesMap 
+					onChangeCaseType={onChangeCaseType}
+				/>
+				<InputRange 
+					value={maxDomainValue} 
+					onChange={handleDomainValueChange}
+					maxDomainValue={maxDomainValue} />
+			<p className={styles.section_title_small}>{`${selectedCaseType} cases`}</p>
 			<div className={styles.map_wrapper}>
 				<ResponsiveMap
 					data={mapData}
 					features={countries.features}
 					margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-					colors={colors[selectedCasesType]}
+					colors={colors[selectedCaseType]}
 					domain={[0, maxDomainValue]}
 					unknownColor='#e9e9e9'
 					valueFormat=',.0f'
@@ -70,5 +65,4 @@ function Map({ data }) {
 		</section>
 	);
 }
-
 export default Map;
