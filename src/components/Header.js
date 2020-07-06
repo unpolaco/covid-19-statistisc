d959a80415248863d977f0bcdd6ce88b2b575e34
Michal Borowski <unpolacoo@gmail.com>
2020-07-06 20:36:12 +0200

    Create scroll event for hide header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
 import styles from './Header.module.scss';
@@ -8,9 +8,37 @@ import InputSearch from './Input_Search';
 export default function Header({ name }) {
 	const headerCountryLinks = ['top', 'chart bar', 'line chart'];
 	const headerGlobalLinks = ['top', 'map', 'summary table'];
+  const [scrollDir, setScrollDir] = useState("scrolling down");
+
+		useEffect(() => {
+			const threshold = 0;
+			let lastScrollY = window.pageYOffset;
+			let ticking = false;
+	
+			const updateScrollDir = () => {
+				const scrollY = window.pageYOffset;
+				if (Math.abs(scrollY - lastScrollY) < threshold) {
+					ticking = false;
+					return;
+				}
+				setScrollDir(scrollY > lastScrollY ? "scrolling down" : "scrolling up");
+				lastScrollY = scrollY > 0 ? scrollY : 0;
+				ticking = false;
+			};
+			const onScroll = () => {
+				if (!ticking) {
+					window.requestAnimationFrame(updateScrollDir);
+					ticking = true;
+				}
+			};
+			window.addEventListener("scroll", onScroll);
+			return () => window.removeEventListener("scroll", onScroll);
+		}, []);
 
 	return (
-		<header className={styles.header}>
+		<header 
+			className={scrollDir==="scrolling up" ? styles.header : styles.hidden}
+			>
 			<ul className={styles.button_list}>
 				{name === 'global'
 					? headerGlobalLinks.map((el) => {
