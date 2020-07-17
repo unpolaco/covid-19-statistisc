511f29226355fc405ecd67bf7153c3f5b320928a
Michal Borowski <unpolacoo@gmail.com>
2020-07-17 17:50:32 +0200

    Updates

---
+++ precache-manifest.ef24c6e9127399f39f1e2df079af3764.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b125a1a8fc21baba206855b18a3948",
+    "url": "/covid-19-statistisc/index.html"
+  },
+  {
+    "revision": "c7995cad25dcdde559d0",
+    "url": "/covid-19-statistisc/static/css/main.2d604bc2.chunk.css"
+  },
+  {
+    "revision": "eaa117a6a6012f5927fb",
+    "url": "/covid-19-statistisc/static/js/2.a61476e5.chunk.js"
+  },
+  {
+    "revision": "3ef5ade87c3c908e84f0a4377ba3007b",
+    "url": "/covid-19-statistisc/static/js/2.a61476e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7995cad25dcdde559d0",
+    "url": "/covid-19-statistisc/static/js/main.f6c52f00.chunk.js"
+  },
+  {
+    "revision": "87450bdf42b23d59756a",
+    "url": "/covid-19-statistisc/static/js/runtime-main.f8589efe.js"
+  }
+]);
